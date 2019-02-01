142f3cfa9c7545d916cb997cf6dc410da0968cb1
Thor Jubera Albo <thor.jubera.albo@gmail.com>
2019-02-01 10:55:00 +0100

    update

--- mongoose-intro/index.js
+++ mongoose-intro/index.js
@@ -1,26 +1,68 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const catSchema = new Schema({
+  name: String,
+  owner: {
+    type: String,
+    required: true
+  },
+  nrOfLegs: {
+    type: Number,
+    default: 4
+  }
+})
 
 // Connect to the database
 mongoose.connect('mongodb://localhost:27017/mongoose-intro', { useNewUrlParser: true });
 
 // Create the models or connect with collection in db
-const Cat = mongoose.model('Cat', { name: String });
-const Fish = mongoose.model('Fish', { name: String });
+const Cat = mongoose.model('Cat', catSchema);
+const Fish = mongoose.model('Fish', { name: String, age: Number });
 
 
-const kitty = new Cat({name: 'Kitty'});
+const kitty = new Cat({
+  name: 'Mia',
+  owner: 'Franci'
+});
 const nemo = new Fish({name: 'Nemo'});
+console.log("Not saved nemo", nemo);
 
-nemo.save()
-  .then((data) => {
-    console.log(data);
-    return Cat.find({})
-  })
+// kitty.save()
+//   .then((data) => {
+//     console.log(data);
+//     return Cat.find({})
+//   })
+//   .then((result) => {
+//     console.log(result);
+//     mongoose.connection.close();
+//   })
+//   .catch(error => {
+//     console.log('There was an error ', error);
+//   })
+
+Fish.create({name: 'Dori'})
   .then((result) => {
-    console.log(result);
-    mongoose.connection.close();
+    console.log('Created fish', result);
   })
   .catch(error => {
-    console.log('There was an error ', error);
+    console.log(error);
   })
 
+Fish.findOne({name: 'Dori'})
+  .then(fish => {
+    fish.age = 2;
+    console.log(fish);
+    fish.save()
+      .then((result) => {
+        console.log('result after saving', result);
+        console.log('saved');
+        mongoose.connection.close();
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  })
+  .catch(err => {
+    console.log(err);
+  })
